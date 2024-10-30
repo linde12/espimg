f281a97ced2454c48e8927baefd13b00cb24f6ad
linde12 <lindeoscar@gmail.com>
2024-10-30 18:53:24 +0100

    lint main.py

--- src/main.py
+++ src/main.py
@@ -1,12 +1,14 @@
-import tempfile
 import random
-from subprocess import Popen
+import tempfile
 from pathlib import Path
+from subprocess import Popen
+
 from fastapi import FastAPI
-from PIL import Image
 from fastapi.responses import FileResponse
+from PIL import Image
 
 app = FastAPI()
+
 
 @app.get("/")
 def generate_picture():
@@ -16,22 +18,23 @@ def generate_picture():
     # stream the image to the client
     return FileResponse(output_path)
 
+
 def get_random_image():
     image_dir = Path("./images")
     images = list(image_dir.glob("*"))
     return random.choice(images)
 
-def transform_picture(path: Path, width: int = 249, height = 122) -> Path:
-    """ Transform the picture into a grayscale (black/white) 249x122 RAW format where each bit
-    represents a pixel value (1/0). Automatically rotates the picture to best fit the aspect ratio """
+
+def transform_picture(path: Path, width: int = 249, height: int = 122) -> Path:
+    """Transform the picture into a grayscale (black/white) 249x122 RAW format where each bit
+    represents a pixel value (1/0). Automatically rotates the picture to best fit the aspect ratio"""
 
     tmp_path = Path(tempfile.mktemp(suffix=path.suffix))
-    print("tmp_path is", tmp_path);
+    print("tmp_path is", tmp_path)
 
     # Trim the image
     p = Popen(["magick", path, "-fuzz", "40%", "-trim", tmp_path])
-    p.wait()
-
+    _ = p.wait()
 
     # Rotate the image if it's taller than it is wide, so that it fits on a landscape screen
     # TODO: Make this respect the input width/height aspect ratio
@@ -41,7 +44,7 @@ def transform_picture(path: Path, width: int = 249, height = 122) -> Path:
     if img_h > img_w:
         print("rotating")
         p = Popen(["magick", tmp_path, "-rotate", "90", tmp_path])
-        p.wait()
+        _ = p.wait()
 
     options = [
         "magick",
@@ -56,13 +59,10 @@ def transform_picture(path: Path, width: int = 249, height = 122) -> Path:
         "-alpha",
         "off",
         "-depth",
-        "1"
+        "1",
     ]
 
     # Perform transformation, print stdout
     p = Popen([*options, f"gray:{tmp_path.with_suffix('.raw')}"])
-    p.wait()
+    _ = p.wait()
     return tmp_path.with_suffix(".raw")
-
-if __name__ == "__main__":
-    transform_picture(Path("./images/anigirl.jpg"))
